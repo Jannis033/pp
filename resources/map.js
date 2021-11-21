00eb7417cdbcdb6e2fad372c799e2f8d371eb23b
Jannis <jannis033@gmail.com>
2021-11-21 20:19:18 +0100

    update

--- resources/map.js
+++ resources/map.js
@@ -22,7 +22,17 @@ let map1 = [
     'W W W W W W W W W W W W W W W W W W W W'
 ];
 
+var pokefunctions = function() {
+    this.f1 = function() {
+        console.log("taubsu");
+    }
+}
+
+var pfnc = new pokefunctions();
+
 var pokelist = new Map();
 
-pokelist.set("1", { name: "Taubsi", text: "Hallo..." });
-pokelist.set("2", { name: "Pikachu", text: "Hey!" });
+pokelist.set("1", { name: "Taubsi", function: pfnc.f1 });
+pokelist.set("2", { name: "Pikachu", function: pfnc.f2 });
+
+pokelist.get("1")["function"](); // run taubsi function example
