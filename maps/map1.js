b571739ef5138d0a508dd4401847d9094d0771d8
Jannis <jannis033@gmail.com>
2021-11-24 22:05:56 +0100

    small map changes

--- maps/map1.js
+++ maps/map1.js
@@ -13,17 +13,17 @@ var map1 = `
     W                   W                                                                                                           V         W
     W                   W                                                                                                           W         W
     W                   W                                                                                                           W         W
-    W                   W             S         S         S           W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W         W 
+    W                   W             S         S         S           W W W W W W W W W W W W W W W W   W W W W W W W W W W W W W W W         W 
     W                   W                                             W                                   W                         W         W
     W                   W                                                                                 W                         W         W
     W                   W                                                                                 W                         W         W
     W W W W W W W W W W W                                             W                                   W                         W         W
     W                   W                                             W                                   W W W W W W W W W W W W W W w W w W W W
-    W                   W             W W W W         W               W                                   W                                     W
-    W                   W             W W W W         W               W                                   W                                     W
-    W                   W             W W W W         W               W                                   W                                     W
-    W                   W             W W W W         W o1o1o1        W                                   W                                     W
-    W                   W             W W W W         W o1o1o1W W W W W                                   W                                     W
+    W                   W             W W W W         W t t t W W W W W                                   W                                     W
+    W                   W             W W W W         W t t t W       W                                   W                                     W
+    W                   W             W W W W         W t t t W       W                                   W                                     W
+    W                   W             W W W W         W o1o1o1W                                           W                                     W
+    W                   W             W W W W         W W W W W W W W W                                   W                                     W
     W                   W             W                               W                                   W                                     W
     W                   W             W                               W                                   W                                     W
     W W W W W W W W W W W             W                               W W W W W W W W W W W W W W W W W W W                                     W
@@ -45,16 +45,16 @@ var map1 = `
     W W W W W W W W W W W             W                                                                                                         W
     W                   W             W                                                                                                         W
     W                   W             W                                                                                                         W
-    W                                 W W W W W W W W W W     W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W     W W W                                                                                                                                                                                                                 W
+    W                                 W W W W W W W W W     W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W     W W W                                                                                                                                                                                                                 W
     W                   W                           W         W         W                                                         W               W
 W W W W W W W W W W W W W                           W         W         W                                                         W               W W W W W W W
-W       W W W W W W W W W t t t t t W o o o W t t t W         W         W                                                         W               W	          W 	
-W       W W W W W W W W W t t t t t W o o o W t t t W         W         W                                                         W                           W 
-W       W W W W W w W W W t t t t t W o o o W t t t W         W         W                                                         W               W           W
+W       W W W W W W W W W t t t t t W t t t W t t t W         W         W                                                         W               W	          W 	
+W       W W W W W W W W W t t t t t W t t t W t t t W         W         W                                                         W                           W 
+W       W W W W W w W W W t t t t t W o3o3o3W t t t W         W         W                                                         W               W           W
 W       W                           W W W W W                 W W W   W W          S          S           S           S           W               W W W W W W W   
-W       W             W             W           P             W         W                                                         W               O
-W       W             W                                                                                                                           O
-W       W W W W W W W W             W                         W         W                                                         W               O
+W       W             W             W           P             W         W                                                         W               T T T O2
+W       W             W                                                                                                                           T T T O2
+W       W W W W W W W W             W                         W         W                                                         W               T T T O2
 W                                   W W w W W W W W W W W W W W         W W W W W W W W W W W W           S           S           W               W W W W W W W
 W                                   W                                   W                     W                                   W               W   W       W
 W                                                                       W                     W                                   W               W   W       W
