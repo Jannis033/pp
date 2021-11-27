4b51295aade33e32c816c78b9f97fbbe512af5e9
LucalucalucalucaLucaluca <93320190+LucalucalucalucaLucaluca@users.noreply.github.com>
2021-11-27 02:09:59 +0100

    Update renderer.js

--- resources/renderer.js
+++ resources/renderer.js
@@ -474,8 +474,8 @@ var EntityDrawer = {
                 context.rect(x + blockSize / 4, y + blockSize / 2, blockSize / 2, blockSize / 2);
                 break;
             case 'c':
-                context.rect(x + blockSize / 4, y + blockSize / 2, blockSize / 2, blockSize / 2);
-                context.rect(x, y + blockSize / 4, blockSize, blockSize / 2);
+                context.rect(x + blockSize / 2, y + blockSize / 4, blockSize / 2, blockSize / 2);
+                context.rect(x + blockSize / 4, y, blockSize / 2, blockSize);
                 break;
             default:
                 context.rect(x, y, blockSize, blockSize);
