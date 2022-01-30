fb88b4dfa4a4c1a82665cfe71dddb6ede627dc9c
LucalucalucalucaLucaluca <93320190+LucalucalucalucaLucaluca@users.noreply.github.com>
2022-01-31 00:24:00 +0100

    Update map1.js
    laby added

--- maps/map1.js
+++ maps/map1.js
@@ -55,21 +55,21 @@ var map1 = `
 WiWiWiWiWiW                                 W W W W W W W W WgB1B1WgW WgWgWgWgW WgWgWgW WgWgWgB1B1WgWgWgW WgWgWgWgB1B1WgWgWgW WgWgWgWgWgW WgWgWgB1B1WgWgW W W W W W W W W               W Wi
 WiWiWiWiWiW                   W                 e2        W         W   ck    Wg  cd        WlWlWlWl            WlWlWlWl                W               W               W               W Wi
 WiW WgWgWgW W W W W W W W W W W               P3          W Wg    WgW   Ot    Wg                                                      cdW               W W W W W W W W W               W Wi
-WiW         W   ck            W ttttttttttW ttttttW ttttttW         W   Ot    Wg                                                        W               W   W       ck  W               W Wi
+WiW   ck    W   ck            W ttttttttttW ttttttW ttttttW         W   Ot    Wg                                                        W               W   W       ck  W               W Wi
 WiWg        W                 W ttttttttttW ttttttW ttttttW         W   Ot    Wg                                                        W               W   W           W               W Wi
 WiWg        W W W W W W W   W W ttttttttttW V2V2V2W ttttttW         W         Wg        W           W           W           W           W                               W               W Wi
 WiWg        Wg                            W W W W W       W         W WgWg  WgW         W           W           W           W           W               W   W           W               W Wi
 WiWg        Wg              Wg            W                         W         Wg                                                        W               W W W W W W W W W               W Wi
 WiW         Wg              Wg            W                         W         Wg                                                        W                 WitTtTtT      W               W Wi
 WiWg        Wg              Wg                                                Wl                                E1                      Wl                WitTtTtT      W               W Wi
-WiWg        W WgWgWgW WgWgWgW             W                         W         Wg                      cd                                W                 WitTtTtT      W               W Wi
-WiWg                                      W W W W W W W W W W W W W W         W WgWgWgWgWgWgWgWgWgWgW           W           W           W               W W W W W W W W W               W Wi
-WiWg                                      W             W           W         Wg                    W Ha                Ot          cd  W               W   W           W               W Wi
-WiW                                                                           Wg    OTOTOT  Ot      W Hm                Ot              W               W   W           W               W Wi
-WiWg                                                                          Wg            Ot      W Hm                Ot              W                         P2  CPW               W Wi
-WiWg                                                                P         e1    OTOTOTOT    cw  W Hm                                W               W   W Cp  Cp  V3W               W Wi
-WiWg                                              OT    OT      OT            Wg                    W He                            ck  W               W W W W W W W W W               W Wi
-WiWg  ck                                                                      Wg                    Wg    cd                            W               W               W               W Wi
+WiWg    WfWfW WgWgWgW WgWgWgW             W                         W         Wg                      cd                                W                 WitTtTtT      W               W Wi
+WiWgXXWf  Wf  WfWf        WfWfWf          W W W W W W W W W W W W W W         W WgWgWgWgWgWgWgWgWgWgW           W           W           W               W W W W W W W W W               W Wi
+WiWg  Wf              Wf      Wf          W             W           W         Wg                    W Ha        W           W       cd  W               W   W           W               W Wi
+WiW     WfWf  Wf    Wf    Wf                                                  Wg    OTOTOT  Ot      W Hm                Ot              W               W   W           W               W Wi
+WiWg  WfWf        Wf        WfWf                                              Wg            Ot      W Hm                Ot              W                         P2  CPW               W Wi
+WiWg      Wf  WfWf    WfWf                                          P         e1    OTOTOTOT    cw  W Hm                                W               W   W Cp  Cp  V3W               W Wi
+WiWgWf  WfWf  Wf    Wf  WfWfWfWf                  OT    OT      OT            Wg                    W He                            ck  W               W W W W W W W W W               W Wi
+WiWgWf          Wf                                                            Wg                    Wg    cd                            W               W               W               W Wi
 WiW WgWgW WgWgWgWgW WgWgWgWgW WgWgWgWgW WgWgWgWgW WgWgWgWgW WgWgWgWgW WgWgWgWgW WgWgWgWgWgW WgWgWgWgW WgWgWgW WgWgWgWgW WgWgWgW WgWgWgWgW WgWgWgW WgWgWgW W W W W W W W W W W W W W W W W Wi
 WiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWiWi
 `
